9ce5940ba9f0fe8383f5029cb099a1af7b4656a0
cpotipat <namsom.chitchanok@gmail.com>
2021-04-22 17:59:56 -0700

    Email me when the price is down to BUY_PRICE

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib
+import os
+
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+
+BUY_PRICE = 85
 
 product_url = "https://www.amazon.com/Passport-Portable-External-Drive-Black/dp/B07VTW2LPX"
 header = {
@@ -14,4 +22,17 @@ result = response.content
 soup = BeautifulSoup(result, "lxml")
 price = soup.find(id="priceblock_ourprice").getText()
 price_without_currency = float(price.split("$")[1])
-print(price_without_currency)
+product_title = soup.find(id="productTitle").get_text().strip()
+
+
+if price_without_currency < BUY_PRICE:
+    message = f"{product_title} is now {price}"
+
+    with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+        connection.starttls()
+        result = connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{product_url}"
+        )
